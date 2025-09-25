224b0a7aba5dc009119bbfb9669093f2535418e9
juicybob123 <okruh2028@kehillahstudent.org>
2025-09-25 08:46:22 -0700

    aaa

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
 import turtle
+x = 1
+# to make able to reflect
+turtle.tracer(0, 0)
 
-turtle.forward(100)
+y = 2
+# If y = 1 it draws one side of the court.
+# If y = 2 it drwas the full court.
+def draw_half_circle():
+    for loop in range (180):
+        turtle.left(1* x)
+        turtle.backward(1 * x)
+for loop in range (y):
+    turtle.right(90 * x)
+    turtle.penup()
+    turtle.forward(250 * x)
+    turtle.backward(500* x)
+    turtle.forward(200*x)
+    turtle.left(90 * x)
+    turtle.pendown()
+    draw_half_circle()
+    turtle.penup()
+    turtle.setpos((-400)* x,50)
+    turtle.pendown()
+    turtle.backward(200*x)
+    turtle.right(270*x)
+    turtle.forward(100*x)
+    turtle.right(90*x)
+    turtle.forward(200*x)
+    turtle.backward(200*x)
+    for loop in range (360):
+        turtle.forward(0.87*x)
+        turtle.right(1*x)
+    turtle.forward(200*x)
+    turtle.right(90*x)
+    turtle.forward(350*x)
+    turtle.backward(600*x)
+    turtle.forward(100*x)
+    turtle.left(90*x)
+    turtle.backward(100*x)
+    for loop in range (180):
+        turtle.backward(3.5*x)
+        turtle.left(1*x)
+    turtle.backward(103*x)
+    turtle.left(90*x)
+    turtle.forward(100*x)
+    turtle.right(90*x)
+    turtle.forward(400*x)
+    turtle.right(90*x)
+    turtle.forward(600*x)
+    turtle.right(90*x)
+    turtle.forward(400*x)
+    turtle.backward(400*x)
+    turtle.left(90*x)
+
+    turtle.setpos(0,0)
+    x = -1
+    turtle.left(90)
+
+
+
+
+turtle.update()
+
+turtle.exitonclick()
--- readme.md
+++ readme.md
@@ -2,17 +2,21 @@
 # Project Proposal
 
 1. Roughly, what will your drawing look like
-
+i want to make a basketball court
 
 2. What are two variables you could use so that when you change each variable, the drawing changes?
 
-
+to make the parobla shape i will need to make a variable that changes and also im gonna repeat the whole code twice but change a variable that is next to each number that changes negetive to duplicate them
 3. Where is there repetition in your drawing?
-
+i will repeat to make circle and also put it on the other side
 
 4. What are the main parts of your drawing? Are there clear sections or objects?
-
+there are clear sections where the 
 
 5. How could you use a loop variable of a for loop? Is there somewhere in your drawing where something is repeated, but it's slightly different each time? Maybe the same object is repeated in different places, or maybe a similar object is repeated but with different sizes?
 
+i would first repeat a lot of stuff to make a circles and half circles for the court and after that i would repeat the whole thing to be on the other side
 
+6. sketch your image, take a pciture, and add it here!
+
+![](sketch.png)
